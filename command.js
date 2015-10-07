306cc8dfc736651e0b7d4b6fbf171d434d1ebb2b
secret_online <me+github@secretonline.co>
2015-10-07 13:53:38 +1300

    Update test script, finally

--- command.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var irc = require('irc');
-var bot = require('./bot');
-var settings = JSON.parse(fs.readFileSync('settings.json'));
-
-bot.get('report', ['an', 'error', 'message', 'man\'s ', 'sky'], 'command line test').forEach(function(item) {
-  console.log(item);
-});
---
+++ test.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+var reload = require('require-reload')(require);
+var bot = reload('./bot.js');
+
+var settings = JSON.parse(fs.readFileSync('settings.json'));
+readConsole();
+
+function reloadBot() {
+  try {
+
+    bot = reload('./bot.js');
+    bot.externalFunctions = {
+      'reloadBot': reloadBot,
+      'isAdmin': isAdmin
+    };
+  } catch (e) {
+    addToReportLog(['failed to reload'], 'bot', false);
+  }
+}
+
+function isAdmin(nick) {
+  return true;
+}
+
+function sendArray(arr, replyTo, settings) {
+  // Send array one item at a time
+  arr.forEach(function(reply) {
+    console.log(reply);
+  });
+}
+
+function readConsole() {
+  var readline = require('readline');
+  var rl = readline.createInterface(process.stdin, process.stdout);
+  rl.setPrompt('');
+  rl.prompt();
+  rl.on('line', function(line) {
+    var argArray = line.split(' ');
+    var reply = bot.getText(argArray, {
+      from: 'nick',
+      to: 'replyTo',
+      callback: sendArray,
+      sendSettings: {}
+    });
+    if (reply.length)
+      sendArray(reply, 'replyTo', {});
+    rl.prompt();
+  }).on('close', function() {
+    process.exit(0);
+  });
+}
